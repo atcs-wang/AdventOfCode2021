814a5af3feb623b221ad29c9d83858744f25bd00
Matthew Wang <matwan@bergen.org>
2021-12-06 14:05:11 -0500

    day04 solutions

---
+++ src/day04/bingo.ts
@@ -0,0 +1,36 @@
+import {BetterGrid, Coordinate} from '../utils/grid';
+
+//undefined is "marked"
+class BingoBoard extends BetterGrid< number | undefined> {
+    
+    mark(num: number) : Coordinate | null {
+        for (let r = 0; r < this.height(); r++){
+            for (let c= 0; c < this.width(); c++){
+                if (this.get(r,c) == num){
+                    this.set(undefined, r,c );
+                    return {row:r, col:c};
+                }
+            }
+        }
+        return null;
+    }
+
+    justWonAt(coord: Coordinate ) : boolean {
+        return (this.get_row(coord).every(num => num === undefined) 
+                || this.get_col(coord).every(num => num === undefined))
+    }
+
+    sumRemaining() : number {
+        // return this.grid.reduce((sum,row) => sum + (row.reduce((s,c) => (s as number) + (c || 0), 0) as number), 0)
+        let sum = 0;
+        for (let r = 0; r < this.height(); r++){
+            for (let c= 0; c < this.width(); c++){
+                sum += this.get(r,c) || 0;
+            }
+        }
+        return sum;
+    }
+
+}
+
+export {BingoBoard}
---
+++ src/day04/part1.ts
@@ -0,0 +1,30 @@
+import * as read_input from '../utils/read_input';
+import {Coordinate} from "../utils/grid"
+import {BingoBoard} from "./bingo";
+
+let chunks : string[][] = read_input.linesAsChunkedStringArray(4);
+
+let nums = chunks[0][0].split(",").map(Number);
+
+let boards : BingoBoard[] = chunks.slice(1).map(chunk => new BingoBoard(chunk, Number));
+
+outerloop:
+for (let num of nums){
+    for (let [index, board] of boards.entries()){
+        
+        let marked : Coordinate | null  = board.mark(num);
+        if (marked !== null && board.justWonAt(marked)){
+            console.log(`Player ${index} won on ${num}; Score: ${num * board.sumRemaining()}`);
+            board.tablePrint();
+            break outerloop;
+        }
+
+    }
+
+}
+
+
+// boards.forEach((b, i) => { 
+//     console.log(`Player ${i}:`);
+//     b.tablePrint()
+// })
